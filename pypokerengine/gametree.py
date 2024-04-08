f0a0f6195258ecf0b4740357a5b54ead97005128
Dilara Tekinoglu <dilaranurtuncturk@gmail.com>
2024-04-08 13:50:20 -0400

    Update gametree.py

--- pypokerengine/gametree.py
+++ pypokerengine/gametree.py
@@ -15,3 +15,92 @@ class PokerNode:
 
     def add_child(self, child):
         self.children.append(child)
+
+class LimitPokerTree:
+    SMALL_BLIND = 10
+    BIG_BLIND = 20
+    MAX_RAISES = 4
+
+    def __init__(self, my_hole_cards, deck):
+        self.root = None
+        self.my_hole_cards = my_hole_cards
+        self.deck = deck  # Instance of Deck class, already shuffled and with known cards removed
+        self.current_pot = self.SMALL_BLIND + self.BIG_BLIND
+
+    def build_tree(self):
+        self.root = PokerNode(player='small_blind', pot=self.current_pot, hole_cards=self.my_hole_cards)
+        self.process_player_decision(self.root, self.my_hole_cards, [])
+        return self.root
+
+
+    def process_player_decision(self, parent_node):
+        # Generate possible actions for a player: fold, call, raise (if raises are left)
+        actions = ['fold', 'call']
+        if parent_node.action != 'raise' or parent_node.amount < self.MAX_RAISES:
+            actions.append('raise')
+
+        for action in actions:
+            if action == 'fold':
+                # Add a fold node and no further children
+                fold_node = PokerNode(player=parent_node.player, action='fold', pot=parent_node.pot, parent=parent_node)
+                parent_node.add_child(fold_node)
+            else:
+                pot_after_action = parent_node.pot
+                if action == 'call':
+                    pot_after_action += self.BIG_BLIND
+                elif action == 'raise':
+                    pot_after_action += self.BIG_BLIND  # Assuming a fixed raise amount
+
+                # Create a node for the action
+                action_node = PokerNode(player=parent_node.player, action=action, pot=pot_after_action, parent=parent_node, amount=parent_node.amount + (1 if action == 'raise' else 0))
+                parent_node.add_child(action_node)
+                
+                # Determine the next player's action or proceed to nature's turn
+                next_player = 'big_blind' if parent_node.player == 'small_blind' else 'small_blind'
+                if next_player == 'big_blind' and action == 'call':
+                    # Nature's turn to deal the flop
+                    self.deal_community_cards(action_node, 3)
+                else:
+                    # Continue with the next player's decision
+                    self.process_player_decision(action_node)
+
+
+    def deal_community_cards(self, parent_node, num_cards, excluded_cards):
+        community_cards = self.deck.draw_cards(num_cards)
+        community_node = PokerNode(player='nature', community_cards=community_cards, parent=parent_node, is_chance_node=True)
+        parent_node.add_child(community_node)
+
+        # Exclude the known cards from the deck for further community card dealing
+        for card in excluded_cards:
+            if card in self.deck.deck:
+                self.deck.deck.remove(card)
+
+
+        # After dealing, proceed based on the round
+        if round_name == 'flop':
+            # The 'flop' has been dealt, proceed to betting round for the 'flop'
+            self.process_player_decision(community_node, self.my_hole_cards, community_cards, 'turn')
+        elif round_name == 'turn':
+            # The 'turn' card has been dealt, proceed to betting round for the 'turn'
+            self.process_player_decision(community_node, self.my_hole_cards, community_cards, 'river')
+        elif round_name == 'river':
+            # The 'river' card has been dealt, this is the final betting round
+            self.process_player_decision(community_node, self.my_hole_cards, community_cards, 'showdown')
+
+
+    def process_player_decision(self, parent_node, hole_cards, community_cards, next_round_name):
+        # Generate possible actions for a player: check, call, bet, fold
+        # Depending on the current round and the state of the game
+        # ...
+
+        # After all actions have been added to the parent_node, check for the next round
+        if next_round_name == 'turn':
+            # Deal the 'turn' card if we are currently in the 'flop' round
+            self.deal_community_cards(parent_node, 1, hole_cards + community_cards, 'turn')
+        elif next_round_name == 'river':
+            # Deal the 'river' card if we are currently in the 'turn' round
+            self.deal_community_cards(parent_node, 1, hole_cards + community_cards, 'river')
+        elif next_round_name == 'showdown':
+            # Handle showdown logic here if we are currently in the 'river' round
+            self.handle_showdown(parent_node, hole_cards, community_cards)
+
