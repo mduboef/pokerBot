9a1a796c9e610cae2ff77aa2481effc1e9938c98
tmead <tmead@umass.edu>
2024-04-19 23:07:12 -0400

    Swapped from laptop to PC

---
+++ pypokerengine/handprobability.py
@@ -0,0 +1,14 @@
+import engine.hand_evaluator
+from engine.hand_evaluator import *
+
+def testProbs(hand):
+    print(hand)
+
+def simulateHands(holeCards, commCards):
+    return
+
+if __name__ == '__main__':
+    testProbs(['CQ', 'D5'])
+    #handEval.HandEvaluator.__is_onepair(['C2', 'C3', 'D3'])
+    test = engine.hand_evaluator.HandEvaluator()
+    test.__is_onepair(['D6', 'C6'])
