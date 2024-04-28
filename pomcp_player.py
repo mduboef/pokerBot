c7d87e57419447bbb0058c6f70afa3d4435b5275
MasonN808 <mason.naka@gmail.com>
2024-04-28 14:31:09 -0400

    more edits

--- pomcp_player.py
+++ pomcp_player.py
@@ -7,11 +7,10 @@ class POMCPPlayer(BasePokerPlayer):
 
   def declare_action(self, valid_actions, hole_card, round_state):
     # TODO: This is where pomcp policy will be
-    # What is uuid?
     # What exactly is stack? the players' amount or players amount betted or is that pot?
     # what is dealer_btn?
     # what is next_player?
-    
+
     pp = pprint.PrettyPrinter(indent=2)
     print("------------ROUND_STATE(RANDOM)--------")
     pp.pprint(round_state)
