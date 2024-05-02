0ab02e419d41fba2bce97ae8d5ef80c4b15a252c
MasonN808 <mason.naka@gmail.com>
2024-05-02 14:39:27 -0400

    made mcts player

--- mcts_player.py
+++ mcts_player.py
@@ -1,3 +1,5 @@
+import json
+from MCTS_poker.utils import State
 from pypokerengine.players import BasePokerPlayer
 from time import sleep
 import pprint
@@ -6,41 +8,43 @@ import pickle
 from MCTS_poker.populate import SearchTree
 import random as rand
 
+
 class MCTSPlayer(BasePokerPlayer):
     def __init__(self):
         super().__init__()
-        self.nodes = self.load_nodes()
-        # print(len(self.nodes))        
+        self.state_actions = self.load_nodes()
+        self.in_table = 0
+        self.not_in_table = 0
+
 
     def load_nodes(self):
-        with open('search_tree.pkl', 'rb') as f:
-            nodes = Pickle.load(f)
-        return nodes
-
-    def find_states(self, hole_card_main, community_cards):
-        matching_states = {}
-        for state, value in self.nodes.items():
-            if state.matches(hole_card_main, community_cards):
-                matching_states[state] = value
-        return matching_states
+        with open('search_tree_40M_sorted.json', 'r') as f:
+            state_actions = json.load(f)
+        return state_actions
 
     def declare_action(self, valid_actions, hole_card, round_state):
         # Check if current observation is a valid state in search tree
-        possible_states = self.find_states(hole_card_main=hole_card, community_cards=round_state["community_card"])
-        print(possible_states)
-        # self.nodes[state]
-        # if 
-        r = rand.random()
-        if r <= 0.5:
-            call_action_info = valid_actions[1]
-        elif r<= 0.9 and len(valid_actions ) == 3:
-            call_action_info = valid_actions[2]
+        state = State.get_state_info_str(hole_cards=hole_card, community_cards=round_state["community_card"])
+        if state in self.state_actions.keys():
+            self.in_table += 1
+            print(f"==>> self.in_table: {self.in_table}")
+            print(f"==>> self.not_in_table: {self.not_in_table}")
+            return self.state_actions[state]
+        # Do a random action
         else:
-            call_action_info = valid_actions[0]
-        action = call_action_info["action"]
-        return action  # action returned here is sent to the poker engine
-
-        return "fold" # action returned here is sent to the poker engine
+            self.not_in_table += 1
+            r = rand.random()
+            if r <= 0.5:
+                call_action_info = valid_actions[1]
+            elif r<= 0.9 and len(valid_actions ) == 3:
+                call_action_info = valid_actions[2]
+            else:
+                call_action_info = valid_actions[0]
+            action = call_action_info["action"]
+            print(f"==>> self.in_table: {self.in_table}")
+            print(f"==>> self.not_in_table: {self.not_in_table}")
+            return action  # action returned here is sent to the poker engine
+        
     
     def receive_game_start_message(self, game_info):
         pass
